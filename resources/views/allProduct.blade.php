4ddfbf7b068343d0b84385ab650516245e5413c6
Gimhana <gimhanak2003@gmail.com>
2026-05-27 16:27:23 +0530

    updated product page UI

--- resources/views/allProduct.blade.php
+++ resources/views/allProduct.blade.php
@@ -20,64 +20,62 @@
 <body>
     <!-- Navbar Section Start -->
     <nav class="navbar navbar-expand-lg custom-navbar sticky-top">
-        <div class="container">
+    <div class="container-fluid px-4">
 
-            <!-- Logo + Brand -->
-            <a class="navbar-brand d-flex align-items-center" href="#">
-                <img src="{{ asset('images/nav-logo.png') }}" alt="logo" class="logo">
-                <div class="ms-2">
-                    <h5 class="mb-0 text-white">Wasana Bakers</h5>
-                    <small class="text-light">Online Cake Delivery</small>
-                </div>
-            </a>
-
-            <!-- Toggle Button -->
-            <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                ☰
-            </button>
-
-            <!-- Menu -->
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <ul class="navbar-nav align-items-center">
-
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="{{ url('/') }}">Home</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="{{ url('/products') }}">Products</a>
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Contact Us</a>
-                    </li>
-
-                    <!-- Order Button -->
-                    <li class="nav-item ms-3">
-                        <a class="btn order-btn" href="{{ url('/products') }}">Order Now</a>
-                    </li>
-
-                    <!-- Login Icon -->
-                    <li class="nav-item ms-3 text-white">
-                        <button class="btn nav-btn-login"><i class="bi bi-person-circle"></i> Log In</button>
-                    </li>
-
-                    <!-- Cart -->
-                    <li class="nav-item ms-3 text-white">
-                        <button type="button" class="btn position-relative nav-cart-btn">
-                            <i class="bi bi-cart4"></i>
-                            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill">
-                                99+
-                                <span class="visually-hidden">unread messages</span>
-                            </span>
-                        </button>
-                    </li>
-
-                </ul>
+        <!-- Logo + Brand -->
+        <a class="navbar-brand d-flex align-items-center" href="#">
+            <img src="{{ asset('images/nav-logo.png') }}" alt="logo" class="logo">
+            <div class="ms-2">
+                <h5 class="mb-0">Wasana Bakers</h5>
+                <small>Online Cake Delivery</small>
             </div>
+        </a>
 
+        <!-- Toggle -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            ☰
+        </button>
+
+        <!-- Menu -->
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav align-items-center">
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/') }}">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="">Products</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+
+                <li class="nav-item ms-3">
+                    <a class="btn order-btn" href="{{ url('/All_Products') }}">Order Now</a>
+                </li>
+
+                <li class="nav-item ms-3">
+                    <button class="btn nav-btn-login">
+                        <i class="bi bi-person-circle"></i> Log In
+                    </button>
+                </li>
+
+                <li class="nav-item ms-3">
+                    <button class="btn position-relative nav-cart-btn">
+                        <i class="bi bi-cart4"></i>
+                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill">
+                            99+
+                        </span>
+                    </button>
+                </li>
+
+            </ul>
         </div>
-    </nav>
+
+    </div>
+</nav>
     <!-- Navbar Section End -->
      <!-- Products Section Start -->
 <div class="products-page">
@@ -148,69 +146,82 @@
             <!-- Copy this card 30 times -->
 
             <div class="product-card">
-                <img src="https://picsum.photos/300/300?random=1">
-                <h3 class="">P 01</h3>
-                <p>LKR 3,000.00</p>
+                <a href="{{ url('/') }}" class="product-card-link">
+                    <img src="{{ asset('images/G 09.jpg') }}">
+                    <h3 class="">P 01</h3>
+                    <p>LKR 3,000.00</p>
+                    <a class="btn product-card-cart">add to cart</a>
+                </a>
             </div>
 
             <div class="product-card">
-                <img src="https://picsum.photos/300/300?random=2">
+                <img src="{{ asset('images/G 11.jpg') }}">
                 <h3>P 02</h3>
                 <p>LKR 2,500.00</p>
+                <a class="btn product-card-cart">add to cart</a>
             </div>
 
             <div class="product-card">
-                <img src="https://picsum.photos/300/300?random=3">
+                <img src="{{ asset('images/G 13.jpg') }}">
                 <h3>P 03</h3>
                 <p>LKR 4,000.00</p>
+                <a class="btn product-card-cart">add to cart</a>
             </div>
 
             <div class="product-card">
-                <img src="https://picsum.photos/300/300?random=4">
+                <img src="{{ asset('images/G 20.jpg') }}">
                 <h3>P 04</h3>
                 <p>LKR 1,800.00</p>
+                <a class="btn product-card-cart">add to cart</a>
             </div>
 
             <div class="product-card">
-                <img src="https://picsum.photos/300/300?random=5">
+                <img src="{{ asset('images/G 27.jpg') }}">
                 <h3>P 05</h3>
                 <p>LKR 5,000.00</p>
+                <a class="btn product-card-cart">add to cart</a>
             </div>
 
             <div class="product-card">
-                <img src="https://picsum.photos/300/300?random=6">
+                <img src="{{ asset('images/G 36.jpg') }}">
                 <h3>P 06</h3>
                 <p>LKR 2,000.00</p>
+                <a class="btn product-card-cart">add to cart</a>
             </div>
 
             <div class="product-card">
-                <img src="https://picsum.photos/300/300?random=7">
+                <img src="{{ asset('images/G 47.jpg') }}">
                 <h3>P 07</h3>
                 <p>LKR 2,700.00</p>
+                <a class="btn product-card-cart">add to cart</a>
             </div>
 
             <div class="product-card">
                 <img src="https://picsum.photos/300/300?random=8">
                 <h3>P 08</h3>
                 <p>LKR 3,200.00</p>
+                <a class="btn product-card-cart">add to cart</a>
             </div>
 
             <div class="product-card">
                 <img src="https://picsum.photos/300/300?random=9">
                 <h3>P 09</h3>
                 <p>LKR 6,000.00</p>
+                <a class="btn product-card-cart">add to cart</a>
             </div>
 
             <div class="product-card">
                 <img src="https://picsum.photos/300/300?random=10">
                 <h3>P 10</h3>
                 <p>LKR 2,900.00</p>
+                <a class="btn product-card-cart">add to cart</a>
             </div>
 
             <div class="product-card">
                 <img src="https://picsum.photos/300/300?random=11">
                 <h3>P 11</h3>
                 <p>LKR 4,500.00</p>
+                <a class="btn product-card-cart">add to cart</a>
             </div>
 
             <div class="product-card">
@@ -275,5 +286,7 @@
 
 </div>
 <!-- Products Section End -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
